b8f2e6673e7bfdde2ef321e2ba2c71936cf5fa1f
Mariana Castro <fercastro.mariana@gmail.com>
2024-01-31 12:34:58 -0300

    pequena atualização em card title; instalação da biblioteca primevue e presets respectivos (wind)

--- src/components/Titles/CardTitle.vue
+++ src/components/Titles/CardTitle.vue
@@ -1,10 +1,15 @@
 <template>
-   <span class="uppercase text-xl font-medium text-silver-900">{{ title }}</span> 
+   <span :class="twMerge('uppercase text-2xl font-medium text-silver-900', styling)">{{ title }}</span> 
 </template>
 
 <script setup lang="ts">
+import { twMerge } from 'tailwind-merge';
 defineProps({
     title: {
+        type: String,
+        default: ''
+    },
+    styling: {
         type: String,
         default: ''
     }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import './style.css'
 import './assets/styles/creatoDisplay.scss';
 
+import PrimeVue from 'primevue/config';
+import Wind from './presets/wind';
+
 import * as components from './components';
 import { App } from 'vue';
 
 export default function install (app: App) {
+  app.use(PrimeVue, {
+    unstyled: true,
+    pt: Wind
+});
   for (const key in components) {
     // @ts-expect-error
     app.component(key, components[key])
